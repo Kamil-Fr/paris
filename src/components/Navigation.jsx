08e4d78f883bf6c24e091a6099ceace526879dec
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2023-08-20 01:17:23 +0200

    Change Header and Navigation.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import '../scss/_navigation.scss';
-import aboutImage from '../images/1.png'
+import { NavigationItems } from "../data/NavigationItems.jsx";
+import { Link } from "react-router-dom";
 
 function Navigation() {
+    const [menuClicked, setMenuClicked] = useState(false)
+
+    const toggleMenu = () => {
+        setMenuClicked(!menuClicked);
+    }
+
     return (
-        <nav className="navigation">
-            <ul>
-                <li><img src={aboutImage} alt="About" /> About</li>
-                <li><img src={aboutImage} alt="History" /> History</li>
-                <li><img src={aboutImage} alt="Recommendations" /> Recommendations</li>
-                <li><img src={aboutImage} alt="Planner" /> Planner</li>
-                <li><img src={aboutImage} alt="Your Account" /> Your Account</li>
+        <nav className="header">
+            <h1 className="header-logo">Paris visit</h1>
+            <div className={`menu-icon`} onClick={toggleMenu}>
+                <FontAwesomeIcon icon={menuClicked ? faTimes : faBars} />
+            </div>
+            <ul className={`navigation ${menuClicked ? 'navigation active' : 'navigation'}`}>
+                {NavigationItems.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <a className={item.clName}>{item.title}</a>
+                        </li>
+                    )
+                })}
+                <button>My Account</button>
             </ul>
         </nav>
     );
