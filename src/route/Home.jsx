d851efba3ab28ac59450c4ec6718aa2adba499b3
Kamil-Fr <kamil.korzeniowski.fr@gmail.com>
2023-10-25 16:53:42 +0200

    Change Home.

--- src/route/Home.jsx
+++ src/route/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
         <>
             <Navigation/>
             <MainView
-            clName="mainView"
+            clName="mainView mainView-home"
             mainImg="https://plus.unsplash.com/premium_photo-1672252617591-cfef963eeefa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80"
             title="Paris visit"
             text="It is time to plan a trip!"
